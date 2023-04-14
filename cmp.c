e84bd3d7bda1d8c871d273e5a29cc3bff4488473
RenanaTurgeman <renanaturgeman058@gmail.com>
2023-04-14 13:25:46 +0300

    add coments to cmp

--- cmp.c
+++ cmp.c
@@ -1,3 +1,7 @@
+// for run:
+//  gcc cmp.c -o cmp
+// ./cmp file1.txt file2.txt -v -i
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,10 +9,11 @@
 
 #define BUF_SIZE 1024
 
+/* Function to compare two files and check if they are equal or not */
 int compare_files(char* file1, char* file2, int ignore_case, int verbose) {
     FILE* fp1 = fopen(file1, "r");
     FILE* fp2 = fopen(file2, "r");
-    if (!fp1 || !fp2) {
+    if (!fp1 || !fp2) { // Check if files were opened successfully
         printf("Error opening files.\n");
         return -1;
     }
@@ -18,6 +23,7 @@ int compare_files(char* file1, char* file2, int ignore_case, int verbose) {
     int line_num = 1;
     int is_equal = 1;
     while (fgets(buf1, BUF_SIZE, fp1) != NULL && fgets(buf2, BUF_SIZE, fp2) != NULL) {
+        /* If case-insensitive flag is on, convert characters to lowercase */
         if (ignore_case) {
             int len = strlen(buf1);
             for (int i = 0; i < len; i++) {
@@ -28,16 +34,18 @@ int compare_files(char* file1, char* file2, int ignore_case, int verbose) {
                 buf2[i] = tolower(buf2[i]);
             }
         }
+        /* Compare two lines and check if they are equal or not */
         if (strcmp(buf1, buf2) != 0) {
             is_equal = 0;
-            if (verbose) {
-                printf("Line %d is distinct.\n", line_num);
-            }
+            // if (verbose) {
+            //     printf("Line %d is distinct.\n", line_num);
+            // }
             break;
         }
         line_num++;
     }
 
+    /* Check if files are equal or not */
     if (is_equal && fgets(buf2, BUF_SIZE, fp2) == NULL) {
         if (verbose) {
             printf("The files are equal.\n");
@@ -50,14 +58,13 @@ int compare_files(char* file1, char* file2, int ignore_case, int verbose) {
         return 1;
     }
 
-    fclose(fp1);
-    fclose(fp2);
+    fclose(fp1); //Close file1
+    fclose(fp2); //Close file2
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        //This line checks if the number of command line arguments is less than three, 
-        //which means that the required arguments (file1 and file2) have not been provided. 
+    /* Check if the required arguments have been provided */
+    if (argc < 3) { //we must have at least 3 arguments
         printf("Usage: cmp <file1> <file2> [-v] [-i]\n");
         return -1;
     }
@@ -65,15 +72,17 @@ int main(int argc, char* argv[]) {
     int ignore_case = 0;
     int verbose = 0;
 
+    /* Check if additional flags are provided */
     for (int i = 3; i < argc; i++) {
-        if (strcmp(argv[i], "-i") == 0) {
+        if (strcmp(argv[i], "-i") == 0) { //If -i flag is provided, set ignore_case flag to 1
             ignore_case = 1;
-        } else if (strcmp(argv[i], "-v") == 0) {
+        } else if (strcmp(argv[i], "-v") == 0) { //If -v flag is provided, set verbose flag to 1
             verbose = 1;
         }
     }
 
+    /* Compare two files and get the result */
     int result = compare_files(argv[1], argv[2], ignore_case, verbose);
 
-    return result;
+    return result; // Return the result of comparison
 }
